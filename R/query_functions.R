d8ebda43daaccdfa6b520364e8b32cf7ca0ed0ed
aasiyahrashan <a.rashan@me.com>
2023-08-17 13:59:30 +0100

    dynamic query works

--- R/query_functions.R
+++ R/query_functions.R
@@ -16,88 +16,46 @@ postgres_connect <- function(host, dbname, port, user, password){
 }
 
 #' Queries the postgres database to get variables required for the severity score specified.
-#' Fixes the postgres timezone issue.
 #' @param postgres_conn A connection object to the postgres database
 #' @param schema The name of the schema you want to query.
-#' @param start_date The date/datetime of the first admission. Needs to be in character format.
+#' @param start_date The earliest ICU admission date/datetime. Needs to be in character format.
 #' @param end_date As above, but for last date
+#' @param min_day The number of days since ICU admission to get physiology data for. Starts with 0
+#' @param max_day The number of days since ICU admision to get physiology data for.
 #' @param dataset_name Describes which concept codes to select. Can only be CCHIC at the moment.
 #' @param severity_score The name of the severity score to calculate. Only APACHE2 at the moment.
 #'
 #' @import lubridate
 #' @import DBI
-#' @import tidyverse
+#' @import dplyr
 #' @import glue
 get_score_variables <- function(postgres_conn, schema, start_date, end_date,
-                                 dataset_name, severity_score){
+                                min_day, max_day, dataset_name, severity_score){
 
-
-  #### Getting the list of concept IDs required for the score.
+  #### Getting the list of concept IDs required for the score, and creating SQL lines from them.
   concepts <- read_csv(system.file(glue("{dataset_name}_concepts.csv"),
                           package = "SeverityScoresOMOP")) %>%
-    filter(score == severity_score)
+    filter(score == severity_score) %>%
+    mutate(max_query = glue(
+      ", MAX(CASE WHEN m.measurement_concept_id = {concept_id} then m.value_as_number END) AS max_{short_name}"),
+      min_query = glue(
+        ", MIN(CASE WHEN m.measurement_concept_id = {concept_id} then m.value_as_number END) AS min_{short_name}"))
 
-  concept_ids <- concepts %>%
-    summarise(concept_ids = toString(concept_id)) %>%
-    pull(concept_ids)
+  ## Collapsing all the queries into a string.
+  variables_required <- glue(glue_collapse(concepts$max_query, sep = "\n"), "\n",
+                      glue_collapse(concepts$min_query, sep = "\n"))
 
-  ###### Creating a query to convert the table from long to wide
-  # Construct the dynamic SQL query to pivot the table
-  pivot_query <- glue::glue("
-  SELECT {id_column},
-         {paste(paste('MAX(CASE WHEN', {{variable_column}}, '=', '''', var, '''', 'THEN', {{value_column}}, 'END) AS', var), collapse = ', ')}
-  FROM {long_table}
-  GROUP BY {id_column}
-")
-
-  pivot_query <- sprintf("
-  SELECT %s,
-         %s
-  FROM %s
-  GROUP BY %s
-", id_column,
-    paste(paste('MAX(CASE WHEN', variable_column, '=', "'", var, "'", 'THEN', value_column, 'END) AS', var), collapse = ', '),
-    long_table,
-    id_column
-  )
-
-  pivot_query <- glue_sql("
-  SELECT {id_column},
-         {glue_collapse(glue('MAX(CASE WHEN {variable_column} = ', .x, ' THEN {value_column} END) AS {var}'), ', ')}
-  FROM {long_table}
-  GROUP BY {id_column}
-", .con = con, .open = "", .sep = ", ", .close = "")
-
-  # Getting the query and substituting parameters.
+  #### Importing the rest of the query from the text file.
   raw_sql <- readr::read_file(system.file("physiology_variables.sql",
                                           package = "SeverityScoresOMOP")) %>%
     glue(schema = schema,
          start_date = start_date,
          end_date = end_date,
-         concept_ids = concept_ids)
+         min_day = min_day,
+         max_day = max_day,
+         variables_required = variables_required)
 
-
-  ###### Trying the variable names query to check it's not supposed to replace properly.
-  variable_names_query <- glue_sql("
-  SELECT DISTINCT {variable_column}
-  FROM {schema_name}.{long_table}
-", .con = postgres_conn)
-
-  variable_names <- dbGetQuery(postgres_conn, as.character(variable_names_query))[[1]]
-
-
-
-#### Getting physiology variables.
-
-  # run the query
-  uds <- dbGetQuery(uds_conn, raw_sql) %>%
-    mutate_if(is.instant, ~fix_timezones_for_imported_dates(.x, source = "star")) %>%
-    mutate_if(is.character, ~str_trim(.)) %>%
-    mutate(date_of_birth = as.Date(date_of_birth),
-           alive = case_when(alive == "FALSE" ~ "Deceased",
-                             alive == "TRUE" ~ "Alive"),
-           sex = case_when(sex == "I" ~ "N",
-                           TRUE ~ sex))
-
-  as_tibble(uds)
+#### Running the query
+  data <- dbGetQuery(postgres_conn, raw_sql)
+  as_tibble(data)
 }
--- inst/physiology_variables.sql
+++ inst/physiology_variables.sql
@@ -1,14 +1,18 @@
 select
-vo.person_id,
-vo.visit_occurrence_id,
-vo.visit_start_datetime,
-DATE_PART('day', m.measurement_datetime - vo.visit_start_datetime) as days_in_icu,
-m.measurement_datetime,
-m.value_as_number
-
+vo.person_id
+, vo.visit_occurrence_id
+, vo.visit_start_datetime
+, DATE_PART('day', m.measurement_datetime - vo.visit_start_datetime) as days_in_icu
+--- List of concept IDs to get the min/max of, along with names to assign them to.
+-- eg MAX(CASE WHEN m.measurement_concept_id = 4301868 then m.value_as_number END) AS max_hr
+{variables_required}
 from {schema}.visit_occurrence vo
 inner join {schema}.measurement m
-
 on vo.visit_occurrence_id = m.visit_occurrence_id
+-- filter by date, and by the number of days after ICU admission
+and vo.visit_start_datetime >= '{start_date}' and vo.visit_start_datetime < '{end_date}'
+and DATE_PART('day', m.measurement_datetime - vo.visit_start_datetime) >= {min_day}
+and DATE_PART('day', m.measurement_datetime - vo.visit_start_datetime) < {max_day}
+-- want min or max values for each visit each day.
+GROUP BY vo.visit_occurrence_id, DATE_PART('day', m.measurement_datetime - vo.visit_start_datetime)
 
-where m.measurement_concept_id IN ({concept_ids})
