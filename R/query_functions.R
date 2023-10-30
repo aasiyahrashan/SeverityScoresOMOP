22a0f5fc84d56bc32213867fedd1c57ebcce540f
Daniel Puttmann <d.p.puttmann@amsterdamumc.nl>
2023-10-30 11:35:56 +0100

    post-merge adjustments

--- R/query_functions.R
+++ R/query_functions.R
@@ -10,25 +10,28 @@
 #' @importFrom RPostgres Postgres
 #' @importFrom odbc odbc
 #' @export
-omop_connect <- function(driver, host, dbname, port, user, password){
-  if (driver == "PostgreSQL"){
-    DBI::dbConnect(
-      RPostgres::Postgres(),
-      host = host,
-      dbname = dbname,
-      port = port,
-      user = user ,
-      password = password)
-  } else {
-    DBI::dbConnect(
-      odbc::odbc(),
-      driver = driver,
-      server = host,
-      database = dbname,
-      uid = user,
-      pwd = password)
+omop_connect <-
+  function(driver, host, dbname, port, user, password) {
+    if (driver == "PostgreSQL") {
+      DBI::dbConnect(
+        RPostgres::Postgres(),
+        host = host,
+        dbname = dbname,
+        port = port,
+        user = user ,
+        password = password
+      )
+    } else {
+      DBI::dbConnect(
+        odbc::odbc(),
+        driver = driver,
+        server = host,
+        database = dbname,
+        uid = user,
+        pwd = password
+      )
+    }
   }
-}
 
 #' Queries a database to get variables required for a specified severity score. Assumes visit detail
 #' table contains ICU admission information. If not available, uses visit_occurrence.
@@ -38,8 +41,8 @@ omop_connect <- function(driver, host, dbname, port, user, password){
 #' @param start_date The earliest ICU admission date/datetime. Needs to be in character format.
 #' @param end_date As above, but for last date
 #' @param min_day The number of days since ICU admission to get physiology data for. Starts with 0
-#' @param max_day The number of days since ICU admission to get physiology data for.
-#' @param dataset_name Describes which concept codes to select. Has to match *_concepts.csv file name.
+#' @param max_day The number of days since ICU admision to get physiology data for.
+#' @param mapping_path Path to *_concepts.csv file containing score to OMOP mappings. Should match the example_concepts.csv file format.
 #' @param severity_score The name of the severity score to calculate. Only APACHE II at the moment.
 #'
 #' @import lubridate
@@ -48,116 +51,158 @@ omop_connect <- function(driver, host, dbname, port, user, password){
 #' @import glue
 #' @import readr
 #' @export
-get_score_variables <- function(conn, driver, schema, start_date, end_date,
-                                min_day, max_day, dataset_name, severity_score){
-  #### Getting the list of concept IDs required for the score, and creating SQL lines from them.
-  concepts <- read_csv(system.file(glue("{dataset_name}_concepts.csv"),
-                                   package = "SeverityScoresOMOP")) %>%
-    filter(score == severity_score)
+get_score_variables <-
+  function(conn,
+           driver,
+           schema,
+           start_date,
+           end_date,
+           min_day,
+           max_day,
+           concepts_file_path,
+           severity_score) {
+    #### Getting the list of concept IDs required for the score, and creating SQL lines from them.
+    concepts <- read_csv(file = concepts_file_path) %>%
+      filter(score == severity_score)
 
-  ##### Getting concepts stored in the measurement table.
-  measurement_concepts <-
-    concepts %>%
-    filter(table == "Measurement") %>%
-    #### GCS can sometimes be stored as a concept ID instead of number.
-    #### These need a separate query.
-    filter(!(short_name %in% c("gcs_eye", "gcs_motor", "gcs_verbal") &
-               omop_variable == "value_as_concept_id")) %>%
-    mutate(max_query = glue(
-      ", MAX(CASE WHEN m.measurement_concept_id = {concept_id} then m.{omop_variable} END) AS max_{short_name}"),
-      min_query = glue(
-        ", MIN(CASE WHEN m.measurement_concept_id = {concept_id} then m.{omop_variable} END) AS min_{short_name}"),
-      unit_query = glue(
-        ", MIN(CASE WHEN m.measurement_concept_id = {concept_id} then c_unit.concept_name END) AS unit_{short_name}"))
-
-  ## Collapsing all the queries into a string.
-  variables_required <- glue(glue_collapse(measurement_concepts$max_query, sep = "\n"), "\n",
-                             glue_collapse(measurement_concepts$min_query, sep = "\n"), "\n",
-                             glue_collapse(measurement_concepts$unit_query, sep = "\n"))
-
-  #### Importing the rest of the query from the text file.
-  #### Using T-SQL if SQL Server
-  if(grep("SQL Server", driver, ignore.case = TRUE)){
-    file_name_physiology_variables <- "physiology_variables_sql_server.sql"
-    }else{
-    file_name_physiology_variables <- "physiology_variables.sql"
-    }
-  raw_sql <- readr::read_file(system.file(file_name_physiology_variables,
-                                          package = "SeverityScoresOMOP")) %>%
-    glue(schema = schema,
-         start_date = start_date,
-         end_date = end_date,
-         min_day = min_day,
-         max_day = max_day,
-         variables_required = variables_required)
-
-  #### Running the query
-  data <- dbGetQuery(conn, raw_sql)
-
-  ######### Doing GCS separately if stored as concept ID instead of number.
-  ######### Otherwise, there's no way of getting the min and max
-  gcs_concepts <- concepts %>%
-    filter((short_name %in% c("gcs_eye", "gcs_motor", "gcs_verbal") &
-              omop_variable == "value_as_concept_id"))
-
-  if(nrow(gcs_concepts > 0)){
-    ###### The SQL file currently has the GCS LOINC concepts hardocded.
-    ###### I plan to construct it from here when I have time.
-    raw_sql <- readr::read_file(system.file("gcs_if_stored_as_concept.sql",
-                                            package = "SeverityScoresOMOP")) %>%
-      glue(schema = schema,
-           start_date = start_date,
-           end_date = end_date,
-           min_day = min_day,
-           max_day = max_day)
-    #### Running the query
-    gcs_data <- dbGetQuery(conn, raw_sql)
-
-    ### Don't like joining here, but not much choice.
-    data <- left_join(data, gcs_data,
-                      by = c("person_id", "visit_occurrence_id",
-                             "visit_detail_id", "days_in_icu"))
-  }
-
-  ########## Some concepts may be stored in the observation table.
-  ########## Using a completely separate query for these and joining the results after
-  ########## TODO - try getting these into one query so the joining can happen in SQL.
-  ########## Other datasets may store numeric values in the observation table. This is not currently supported.
-  observation_concepts <- concepts %>%
-    filter(table == "Observation" & omop_variable == "value_as_concept_id")
-
-  if (nrow(observation_concepts) > 0){
-    #### Most of these use several concepts IDs to represent the same score variable.
-    #### Eg, CCAA uses 3 codes for renal failure. Collapsing those here.
-    #### The query returns the number of rows matching the concept IDs provided.
-    observation_concepts <- observation_concepts %>%
-      group_by(short_name, concept_id) %>%
-      summarise(concept_id_value =
-                  glue("'", glue_collapse(concept_id_value, sep = "', '"), "'")) %>%
-      mutate(count_query = glue(
-        ", COUNT (CASE WHEN o.observation_concept_id = {concept_id}
-        AND o.value_as_concept_id IN ({concept_id_value}) THEN o.observation_id END)
-        as count_{short_name}"))
+    ##### Getting concepts stored in the measurement table.
+    measurement_concepts <-
+      concepts %>%
+      filter(table == "Measurement") %>%
+      #### GCS can sometimes be stored as a concept ID instead of number.
+      #### These need a separate query.
+      filter(!(short_name %in% c("gcs_eye", "gcs_motor", "gcs_verbal") &
+                 omop_variable == "value_as_concept_id")) %>%
+      mutate(
+        max_query = glue(
+          ", MAX(CASE WHEN m.measurement_concept_id = {concept_id} then m.{omop_variable} END) AS max_{short_name}"
+        ),
+        min_query = glue(
+          ", MIN(CASE WHEN m.measurement_concept_id = {concept_id} then m.{omop_variable} END) AS min_{short_name}"
+        ),
+        unit_query = glue(
+          ", MIN(CASE WHEN m.measurement_concept_id = {concept_id} then c_unit.concept_name END) AS unit_{short_name}"
+        )
+      )
 
     ## Collapsing all the queries into a string.
-    observation_variables_required <-
-      glue(glue_collapse(observation_concepts$count_query, sep = "\n"), "\n")
+    variables_required <-
+      glue(
+        glue_collapse(measurement_concepts$max_query, sep = "\n"),
+        "\n",
+        glue_collapse(measurement_concepts$min_query, sep = "\n"),
+        "\n",
+        glue_collapse(measurement_concepts$unit_query, sep = "\n")
+      )
 
     #### Importing the rest of the query from the text file.
-    raw_sql <- readr::read_file(system.file("history_variables.sql",
-                                            package = "SeverityScoresOMOP")) %>%
-      glue(schema = schema,
-           start_date = start_date,
-           end_date = end_date,
-           observation_variables_required = observation_variables_required)
+    #### Using T-SQL if SQL Server
+    if (grep("SQL Server", driver, ignore.case = TRUE)) {
+      file_name_physiology_variables <-
+        "physiology_variables_sql_server.sql"
+    } else{
+      file_name_physiology_variables <- "physiology_variables.sql"
+    }
+    raw_sql <-
+      readr::read_file(system.file(file_name_physiology_variables,
+                                   package = "SeverityScoresOMOP")) %>%
+      glue(
+        schema = schema,
+        start_date = start_date,
+        end_date = end_date,
+        min_day = min_day,
+        max_day = max_day,
+        variables_required = variables_required
+      )
+
     #### Running the query
-    observation_data <- dbGetQuery(conn, raw_sql)
+    data <- dbGetQuery(conn, raw_sql)
 
-    #### Don't like having to merge here, but doing it for now.
-    data <- left_join(data, observation_data,
-                      by = c("person_id", "visit_occurrence_id", "visit_detail_id"))
+    ######### Doing GCS separately if stored as concept ID instead of number.
+    ######### Otherwise, there's no way of getting the min and max
+    gcs_concepts <- concepts %>%
+      filter((
+        short_name %in% c("gcs_eye", "gcs_motor", "gcs_verbal") &
+          omop_variable == "value_as_concept_id"
+      ))
+
+    if (nrow(gcs_concepts > 0)) {
+      ###### The SQL file currently has the GCS LOINC concepts hardocded.
+      ###### I plan to construct it from here when I have time.
+      raw_sql <-
+        readr::read_file(system.file("gcs_if_stored_as_concept.sql",
+                                     package = "SeverityScoresOMOP")) %>%
+        glue(
+          schema = schema,
+          start_date = start_date,
+          end_date = end_date,
+          min_day = min_day,
+          max_day = max_day
+        )
+      #### Running the query
+      gcs_data <- dbGetQuery(conn, raw_sql)
+
+      ### Don't like joining here, but not much choice.
+      data <- left_join(
+        data,
+        gcs_data,
+        by = c(
+          "person_id",
+          "visit_occurrence_id",
+          "visit_detail_id",
+          "days_in_icu"
+        )
+      )
+    }
+
+    ########## Some concepts may be stored in the observation table.
+    ########## Using a completely separate query for these and joining the results after
+    ########## TODO - try getting these into one query so the joining can happen in SQL.
+    ########## Other datasets may store numeric values in the observation table. This is not currently supported.
+    observation_concepts <- concepts %>%
+      filter(table == "Observation" &
+               omop_variable == "value_as_concept_id")
+
+    if (nrow(observation_concepts) > 0) {
+      #### Most of these use several concepts IDs to represent the same score variable.
+      #### Eg, CCAA uses 3 codes for renal failure. Collapsing those here.
+      #### The query returns the number of rows matching the concept IDs provided.
+      observation_concepts <- observation_concepts %>%
+        group_by(short_name, concept_id) %>%
+        summarise(concept_id_value =
+                    glue("'", glue_collapse(concept_id_value, sep = "', '"), "'")) %>%
+        mutate(
+          count_query = glue(
+            ", COUNT (CASE WHEN o.observation_concept_id = {concept_id}
+        AND o.value_as_concept_id IN ({concept_id_value}) THEN o.observation_id END)
+        as count_{short_name}"
+          )
+        )
+
+      ## Collapsing all the queries into a string.
+      observation_variables_required <-
+        glue(glue_collapse(observation_concepts$count_query, sep = "\n"),
+             "\n")
+
+      #### Importing the rest of the query from the text file.
+      raw_sql <- readr::read_file(system.file("history_variables.sql",
+                                              package = "SeverityScoresOMOP")) %>%
+        glue(
+          schema = schema,
+          start_date = start_date,
+          end_date = end_date,
+          observation_variables_required = observation_variables_required
+        )
+      #### Running the query
+      observation_data <- dbGetQuery(conn, raw_sql)
+
+      #### Don't like having to merge here, but doing it for now.
+      data <- left_join(
+        data,
+        observation_data,
+        by = c("person_id", "visit_occurrence_id", "visit_detail_id")
+      )
+    }
+
+    as_tibble(data)
   }
-
-  as_tibble(data)
-}
-
