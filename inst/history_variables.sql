09a6a8dd11cbd4796250fe4ab99baf1c3fc48010
Daniel Puttmann <d.p.puttmann@amsterdamumc.nl>
2023-12-20 23:05:06 +0100

    changed: now CTE based

--- inst/history_variables.sql
+++ inst/history_variables.sql
@@ -14,61 +14,62 @@ AS (
 			  AND (vo.visit_occurrence_id = vd.visit_occurrence_id OR vd.visit_occurrence_id IS NULL)
 	  WHERE COALESCE(vd.visit_detail_start_datetime, vd.visit_detail_start_date, vo.visit_start_datetime, vo.visit_start_date) >= @start_date
 		  AND COALESCE(vd.visit_detail_start_datetime, vd.visit_detail_start_date, vo.visit_start_datetime, vo.visit_start_date) < @end_date
-	  )
-SELECT adm.person_id
-       ,adm.visit_occurrence_id
-       ,adm.visit_detail_id
-       ,count_comorbidity
-       ,count_renal_failure
-       ,count_emergency_admission
-	--- Count queries that describe how many of the comorbidities etc the patient had during that admission. Eg.
-	--- count (case when observation_concept_id = '4214956'
-	--- and value_as_concept_id in ('4267414', '4245975') then observation_id end) as comorbid_number
-FROM icu_admission_details adm
--- making sure the visits match up
+	  ),
+
+/*
+Count queries that describe how many of the comorbidities the patient had during icu admission.
+e.g. COUNT (CASE
+                WHEN observation_concept_id = '4214956'
+                 AND value_as_concept_id IN ('4267414', '4245975')
+                THEN observation_id
+            END ) AS comorbid_number
+*/
+observation_comorbidity
+AS (SELECT person_id
+           ,visit_occurrence_id
+           ,visit_detail_id
+           @observation_variables_required
+      FROM @schema.observation
+  GROUP BY person_id
+           ,visit_occurrence_id
+           ,visit_detail_id
+    ),
+
+condition_comorbidity
+AS (SELECT person_id
+           ,visit_occurrence_id
+           ,visit_detail_id
+           @condition_variables_required
+      FROM @schema.condition_occurrence
+  GROUP BY person_id
+           ,visit_occurrence_id
+           ,visit_detail_id
+    ),
+
+visit_detail_emergency_admission
+AS (SELECT person_id
+           ,visit_occurrence_id
+           ,visit_detail_id
+           @visit_detail_variables_required
+      FROM @schema.visit_detail
+  GROUP BY person_id
+           ,visit_occurrence_id
+           ,visit_detail_id
+    )
+
+    SELECT *
+      FROM icu_admission_details adm
 -- No date filtering in this query because the concept IDs currently used are history/admission specific.
 -- Need to edit the query if this changes for other datasets.
-LEFT JOIN ( SELECT person_id
-                   ,visit_occurrence_id
-                   ,visit_detail_id
-                   @observation_variables_required
-              FROM @schema.observation
-           ) o
-
-	ON adm.person_id = o.person_id
-		AND adm.visit_occurrence_id = o.visit_occurrence_id
-		AND (adm.visit_detail_id = o.visit_detail_id OR adm.visit_detail_id IS NULL)
-
--- making sure the visits match up
--- No date filtering in this query because the concept IDs currently used are history/admission specific.
--- Need to edit the query if this changes for other datasets.
-LEFT JOIN (SELECT person_id
-                  ,visit_occurrence_id
-                  ,visit_detail_id
-                  @condition_variables_required
-             FROM @schema.condition_occurrence
-         GROUP BY person_id
-                  ,visit_occurrence_id
-                  ,visit_detail_id
-           ) AS co
-	ON adm.person_id = co.person_id
-		AND adm.visit_occurrence_id = co.visit_occurrence_id
-		AND (adm.visit_detail_id = co.visit_detail_id OR adm.visit_detail_id IS NULL)
-LEFT JOIN (SELECT person_id
-                  ,visit_occurrence_id
-                  ,visit_detail_id
-                  @visit_detail_variables_required
-             FROM @schema.visit_detail
-         GROUP BY person_id
-                  ,visit_occurrence_id
-                  ,visit_detail_id
-                ) AS vd
-          	ON adm.person_id = vd.person_id
-		AND adm.visit_occurrence_id = vd.visit_occurrence_id
-		AND (adm.visit_detail_id = vd.visit_detail_id)
-GROUP BY adm.person_id
-	     ,adm.visit_occurrence_id
-	     ,adm.visit_detail_id
-       ,count_comorbidity
-       ,count_renal_failure
-       ,count_emergency_admission
+INNER JOIN observation_comorbidity o
+        ON adm.person_id           = o.person_id
+       AND adm.visit_occurrence_id = o.visit_occurrence_id
+       AND adm.visit_detail_id     = o.visit_detail_id
+INNER JOIN condition_comorbidity co
+        ON adm.person_id           = co.person_id
+       AND adm.visit_occurrence_id = co.visit_occurrence_id
+       AND adm.visit_detail_id     = co.visit_detail_id
+INNER JOIN visit_detail_emergency_admission vd
+        ON adm.person_id           = vd.person_id
+       AND adm.visit_occurrence_id = vd.visit_occurrence_id
+       AND adm.visit_detail_id     = vd.visit_detail_id
